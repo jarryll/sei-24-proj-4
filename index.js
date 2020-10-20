ec8ebda08bc96b4c86a4856106475bff8bcfeb6b
jarryl <jarryl.chia@gmail.com>
2020-10-20 11:56:09 +0800

    Fixed issue with backend not serving sign in page files

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use("/api/upload", uploadImgRoute)
 
 if (process.env.NODE_ENV === "production") {
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "client/build/index.html")))
+        res.sendFile(path.join(__dirname, "client/build/index.html"))
     })
 }
 
